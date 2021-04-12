1f08722798076c3333cfa377896248cc298e519e
syang47 <38193793+syang47@users.noreply.github.com>
2021-04-12 02:22:16 -0700

    removed duplicated lines

--- webserver/flaskserver.py
+++ webserver/flaskserver.py
@@ -11,10 +11,6 @@ conn = sqlite3.connect('leaguemate.db')
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
-conn = sqlite3.connect('leaguemate.db')
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-
 #manage logged in users
 login_manager = LoginManager(app)
 
